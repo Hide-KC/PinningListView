979135c5d05e507d378263d85a41cba04886e663
Hide-KC <kcs.dev.labo@gmail.com>
2019-06-22 21:29:35 +0900

    ほぼ初期コミット。RecyclerView導入済

---
+++ pinheaderslistview/src/main/java/work/kcs_labo/pinheaderslistview/PinHeadersListView.kt
@@ -0,0 +1,10 @@
+package work.kcs_labo.pinheaderslistview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class PinHeadersListView: RecyclerView{
+  constructor(context: Context): super(context)
+  constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+}
