daf4c8319863848fa736e06eca0e3de4ac0bd5b7
Hide-KC <kcs.dev.labo@gmail.com>
2019-07-08 22:51:20 +0900

    abstracted PinningListAdapter

---
+++ pinninglistview/src/main/java/work/kcs_labo/pinninglistview/PinningListAdapter.kt
@@ -0,0 +1,35 @@
+package work.kcs_labo.pinninglistview
+
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class PinningListAdapter<E, VH : RecyclerView.ViewHolder>(private val list: List<E>) :
+  RecyclerView.Adapter<VH>(),
+  PinningListDecoration.PinningListListener {
+
+  private var headerLayout: Int? = null
+
+  companion object Constants {
+    const val HEADER = 200
+  }
+
+  override fun isHeader(adapterPosition: Int): Boolean {
+    return getItemViewType(adapterPosition) == HEADER
+  }
+
+  override fun getCurrentHeaderPosition(adapterPosition: Int): Int? {
+    var index = adapterPosition
+    while (index > -1) {
+      if (getItemViewType(index) == HEADER) return index
+      index--
+    }
+    return null
+  }
+
+  override fun getHeaderLayout(): Int? {
+    return headerLayout
+  }
+
+  override fun getItemCount(): Int {
+    return list.size
+  }
+}
