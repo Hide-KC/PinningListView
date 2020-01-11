3e4e69db4f90fe721283fa5c601d1bb8a1b44071
Hide-KC <kcs.dev.labo@gmail.com>
2020-01-12 01:08:08 +0900

    apply Decoration for DataBinding

---
+++ pinninglistview/src/main/java/work/kcs_labo/pinninglistview/PinningListDecorationForDataBinding.kt
@@ -0,0 +1,107 @@
+package work.kcs_labo.pinninglistview
+
+import android.graphics.Canvas
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @param <E> Item object type parameter.
+ * @param <T> Used for confirm Header section (Please set the type parameter matched by E::Property).
+ * @param <H> Header object type parameter.
+ */
+open class PinningListDecorationForDataBinding<E, T, H>(private val listener: PinningListDBListener<E, T, H>) :
+  RecyclerView.ItemDecoration() {
+
+  private var header = Pair<Int?, ViewDataBinding?>(null, null)
+
+  fun getHeaderWidth(): Int? = header.second?.root?.measuredWidth
+
+  fun getHeaderHeight(): Int? = header.second?.root?.measuredHeight
+
+  override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+    super.onDrawOver(c, parent, state)
+
+    val topView = parent.getChildAt(0) ?: return
+    val adapterPosition = parent.getChildAdapterPosition(topView)
+    if (adapterPosition == RecyclerView.NO_POSITION) return
+    val currentHeaderPosition = listener.getCurrentHeaderPosition(adapterPosition) ?: return
+
+    val currentHeader = when (val layoutResId = listener.headerLayout ?: return) {
+      header.first -> {
+        header.second
+      }
+      else -> {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(inflater, layoutResId, parent, false)
+        header = Pair(layoutResId, binding)
+        binding
+      }
+    } ?: return
+
+    fixLayoutSize(parent, currentHeader.root)
+    listener.bindHeaderData(currentHeader, currentHeaderPosition)
+
+    val contactPoint = currentHeader.root.bottom
+    val nextHeaderTop = getNextHeaderPoint(parent)
+
+    if (nextHeaderTop != null
+      && nextHeaderTop <= contactPoint
+      && nextHeaderTop > 0
+    ) {
+      moveHeader(c, currentHeader.root, nextHeaderTop)
+    } else {
+      drawHeader(c, currentHeader.root)
+    }
+  }
+
+  private fun getNextHeaderPoint(parent: RecyclerView): Int? {
+    for (childPosition in 0 until parent.childCount) {
+      val child = parent.getChildAt(childPosition)
+      val adapterPosition = parent.getChildAdapterPosition(child)
+      if (listener.isHeader(adapterPosition)) return child.top
+    }
+    return null
+  }
+
+  private fun drawHeader(c: Canvas, view: View) {
+    c.save()
+    c.translate(0F, 0F)
+    view.draw(c)
+    c.restore()
+  }
+
+  private fun moveHeader(c: Canvas, currentView: View, nextViewTop: Int) {
+    c.save()
+    c.translate(0F, (nextViewTop - currentView.height).toFloat())
+    currentView.draw(c)
+    c.restore()
+  }
+
+  private fun fixLayoutSize(parent: ViewGroup, view: View) {
+    // Specs for parent (RecyclerView)
+    val widthSpec = View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY)
+    val heightSpec = View.MeasureSpec.makeMeasureSpec(parent.height, View.MeasureSpec.UNSPECIFIED)
+
+    // Specs for children (headers)
+    val childWidthSpec =
+      ViewGroup.getChildMeasureSpec(
+        widthSpec,
+        parent.paddingLeft + parent.paddingRight,
+        view.layoutParams.width
+      )
+    val childHeightSpec =
+      ViewGroup.getChildMeasureSpec(
+        heightSpec,
+        parent.paddingTop + parent.paddingBottom,
+        view.layoutParams.height
+      )
+
+    view.measure(childWidthSpec, childHeightSpec)
+
+    view.layout(0, 0, view.measuredWidth, view.measuredHeight)
+  }
+}
--- pinninglistview/src/main/java/work/kcs_labo/pinninglistview/PinningListListener.kt
+++ pinninglistview/src/main/java/work/kcs_labo/pinninglistview/PinningListListener.kt
@@ -1,6 +1,7 @@
 package work.kcs_labo.pinninglistview
 
 import android.view.View
+import androidx.databinding.ViewDataBinding
 
 /**
  * @param <E> Item object type parameter.
@@ -12,4 +13,16 @@ interface PinningListListener<E, T, H> {
   fun isHeader(adapterPosition: Int): Boolean
   fun getCurrentHeaderPosition(adapterPosition: Int): Int?
   fun bindHeaderData(header: View, adapterPosition: Int)
+}
+
+/**
+ * @param <E> Item object type parameter.
+ * @param <T> Used for confirm Header section (Set the type parameter matched by E::Property<T>).
+ * @param <H> Header object type parameter.
+ */
+interface PinningListDBListener<E, T, H> {
+  val headerLayout: Int?
+  fun isHeader(adapterPosition: Int): Boolean
+  fun getCurrentHeaderPosition(adapterPosition: Int): Int?
+  fun bindHeaderData(binding: ViewDataBinding, adapterPosition: Int)
 }
